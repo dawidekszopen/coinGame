63944fbdca4ef8e6ebdcd91c08685874098b4ddf
dawidekszopen <dawid.witan0@gmail.com>
2024-03-06 07:16:40 +0100

    coingame

---
+++ classes.py
@@ -0,0 +1,11 @@
+class player():
+    def __init__(self) -> None:
+        self.name =''
+        self.hp = 100
+        self.attack = 5
+
+
+class enemy():
+    def __init__(self) -> None:
+        self.hp = 100
+        self.attack = 5
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+from pygame_widgets.button import Button
+
+import classes
 
 #https://colorhunt.co/palette/35374b34495550727b78a083 - paleta kolorów
 
+gracz = classes.player()
 
 pygame.init()
 
@@ -10,12 +14,34 @@ pygame.display.set_caption('coinGame')
 
 running = True
 
+
 def main():
     global running
 
     pygame.draw.rect(screen, (53, 55, 75), pygame.Rect(0, 550, 800, 250), border_top_left_radius=20, border_top_right_radius=20)
     pygame.draw.rect(screen, (52, 73, 85), pygame.Rect(38, 566, 333, 220), border_radius=10)
     pygame.draw.rect(screen, (52, 73, 85), pygame.Rect(429, 566, 333, 220), border_radius=10)
+
+    pygame.draw.rect(screen, (217, 217, 217), pygame.Rect(454, 75, 153, 286))
+
+
+    attack = Button(
+        screen,
+        443, 574, 305, 64,
+        text="attack",
+    )
+
+    items = Button(
+        screen,
+        443, 643, 305, 64,
+        text="items",
+    )
+
+    defe = Button(
+        screen,
+        443, 712, 305, 64,
+        text="def",
+    )
 
     while running:
         events = pygame.event.get()
@@ -25,6 +51,10 @@ def main():
                 running = False
 
 
+
+        attack.draw()
+        items.draw()
+        defe.draw()
         pygame.display.update()
 
 
