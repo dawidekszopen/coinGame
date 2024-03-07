8985554fb7e38cdeb7bda74ceda32286e293d552
dawidekszopen <dawid.witan0@gmail.com>
2024-03-07 11:27:35 +0100

    coinGame

--- classes.py
+++ classes.py
@@ -1,4 +1,6 @@
 from random import randint
+from pygame_widgets.button import Button
+from pygame_widgets.progressbar import ProgressBar
 
 class player():
     def __init__(self) -> None:
@@ -23,3 +25,90 @@ class enemy():
 
     def getDmg(self, dmg):
         self.hp -= dmg
+
+
+
+class game():
+    def __init__(self, screenNew) -> None:
+        self.screen = screenNew
+
+        self.doAttack = False
+
+
+        self.attack = Button(
+            self.screen,
+            443, 574, 305, 64,
+            text="attack",
+            onClick= lambda: self.attackCoin(),
+            inactiveColour=(53, 55, 75),
+            pressedColour=(120, 160, 131),
+            hoverColour=(80, 114, 123),
+        )
+
+        self.items = Button(
+            self.screen,
+            443, 643, 305, 64,
+            text="items",
+            inactiveColour=(53, 55, 75),
+            pressedColour=(120, 160, 131),
+            hoverColour=(80, 114, 123),
+        )
+
+        self.defe = Button(
+            self.screen,
+            443, 712, 305, 64,
+            text="def",
+            inactiveColour=(53, 55, 75),
+            pressedColour=(120, 160, 131),
+            hoverColour=(80, 114, 123),
+        )
+
+        self.enemyHp = ProgressBar(
+            self.screen, 
+            454, 50, 154, 20, 
+            lambda: 100 * 0.01
+        )
+
+
+        self.orzel = Button(
+            self.screen,
+            637, 625, 100, 100,
+            radius=100,
+            inactiveColour=(53, 55, 75),
+            pressedColour=(120, 160, 131),
+            hoverColour=(80, 114, 123),
+            text="orzeł"
+        )
+
+        self.reszka = Button(
+            self.screen,
+            454, 625, 100, 100,
+            radius=100,
+            inactiveColour=(53, 55, 75),
+            pressedColour=(120, 160, 131),
+            hoverColour=(80, 114, 123),
+            text="reszka"
+        )
+
+        self.reszka.hide()
+        self.orzel.hide()
+
+    def update(self):
+        self.attack.draw()
+        self.items.draw()
+        self.defe.draw()
+        self.enemyHp.draw()
+        self.reszka.draw()
+        self.orzel.draw()
+
+  
+    def attackCoin(self):
+        self.doAttack = True
+        self.attack.hide()
+        self.items.hide()
+        self.defe.hide()
+
+        self.reszka.show()
+        self.orzel.show()
+
+
--- main.py
+++ main.py
@@ -14,9 +14,11 @@ enemy = classes.enemy(100, 5)
 pygame.init()
 
 screen = pygame.display.set_mode((800, 800))
-wybor = pygame.display.set_mode((800, 800))
 
 pygame.display.set_caption('coinGame')
+
+
+gra = classes.game(screen)
 
 running = True
 
@@ -26,77 +28,20 @@ def main():
 
     pygame.draw.rect(screen, (53, 55, 75), pygame.Rect(0, 550, 800, 250), border_top_left_radius=20, border_top_right_radius=20)
     pygame.draw.rect(screen, (52, 73, 85), pygame.Rect(38, 566, 333, 220), border_radius=10)
-    pygame.draw.rect(screen, (52, 73, 85), pygame.Rect(429, 566, 333, 220), border_radius=10)
+
 
     pygame.draw.rect(screen, (217, 217, 217), pygame.Rect(454, 75, 153, 286))
 
-
-    attack = Button(
-        screen,
-        443, 574, 305, 64,
-        text="attack",
-        onClick= lambda: enemy.getDmg(gracz.attackEnemy()),
-        inactiveColour=(200, 50, 0),
-        pressedColour=(0,200,0)
-    )
-
-    items = Button(
-        screen,
-        443, 643, 305, 64,
-        text="items",
-    )
-
-    defe = Button(
-        screen,
-        443, 712, 305, 64,
-        text="def",
-    )
-
-    enemyHp = ProgressBar(screen, 
-        454, 50, 154, 20, 
-        lambda: enemy.hp * 0.01
-    )
-
-    tlo = pygame.draw.rect(wybor, (120, 160, 131), pygame.Rect(163, 150, 500, 500))
-
-    orzel = Button(
-        wybor,
-        189, 325, 150, 150,
-        radius=100,
-        inactiveColour=(53, 55, 75),
-        pressedColour=(80, 114, 123),
-        hoverColour=(52, 73, 85),
-        text="orzeł"
-    )
-
-    reszka = Button(
-        wybor,
-        485, 325, 150, 150,
-        radius=100,
-        inactiveColour=(53, 55, 75),
-        pressedColour=(80, 114, 123),
-        hoverColour=(52, 73, 85),
-        text="reszka"
-    )
-
-
     while running:
+        pygame.draw.rect(screen, (52, 73, 85), pygame.Rect(429, 566, 333, 220), border_radius=10)
         events = pygame.event.get()
 
         for event in events:
             if event.type == pygame.QUIT:
                 running = False
 
-
-        attack.draw()
-        items.draw()
-        defe.draw()
-        enemyHp.draw()
-
-        orzel.draw()
-        reszka.draw()
         
-       
+        gra.update()
 
         pygame_widgets.update(events)
         pygame.display.update()
